f19f50b40236847fb64054b91efcd0df0bd7f943
Paulo-A <pauloeduardopandrade@gmail.com>
2021-01-05 10:36:38 -0300

    Add event handler by pathname

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -5,48 +5,59 @@
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
 
-document.getElementsByClassName("menu-lista-contato")[0].addEventListener("click", function(){
-    console.log("heyheyhey contact clicking");
-    ga('send', 'event', 'menu', 'entre_em_contato', 'link_externo');
-});
+path = window.location.pathname
+if (path === '/analise'){
+    evento_pagina_analise()
+} else if (path === '/sobre'){
+    evento_pagina_sobre()
+}
+evento_padrao()
 
-document.getElementsByClassName("menu-lista-download")[0].addEventListener("click", function () {
-    console.log("heyheyhey download_pdf clicking");
-    ga('send', 'event', 'menu', 'download_pdf', 'download_pdf');
-});
+function evento_padrao() {
+    document.getElementsByClassName('menu-lista-contato')[0].addEventListener('click', function(){
+        console.log('Contato event sent');
+        ga('send', 'event', 'menu', 'entre_em_contato', 'link_externo');
+    });
 
-card_montadora = document.getElementsByClassName("card-montadoras")
-card_montadora[0].addEventListener("click", function () {
-    console.log("heyheyhey card_montadora[0]");
-    console.log(card_montadora[0].dataset.name)
-    ga('send', 'event', 'analise', 'ver_mais', card_montadora[0].dataset.name);
-});
-card_montadora[1].addEventListener("click", function () {
-    console.log("heyheyhey card_montadora[1]");
-    console.log(card_montadora[1].dataset.name)
-    ga('send', 'event', 'analise', 'ver_mais', card_montadora[1].dataset.name);
-});
-card_montadora[2].addEventListener("click", function () {
-    console.log("heyheyhey card_montadora[2]");
-    console.log(card_montadora[2].dataset.name)
-    ga('send', 'event', 'analise', 'ver_mais', card_montadora[2].dataset.name);
-});
+    document.getElementsByClassName('menu-lista-download')[0].addEventListener('click', function () {
+        console.log('Download event sent');
+        ga('send', 'event', 'menu', 'download_pdf', 'download_pdf');
+    });
+}
 
-form_contato = document.getElementsByClassName("contato")[0]
-form_contato[0].addEventListener("blur", function () {
-    console.log("heyheyhey blur name");
-    ga('send', 'event', 'contato', form_contato.id, 'preencheu');
-});
-form_contato[1].addEventListener("blur", function () {
-    console.log("heyheyhey blur email");
-    ga('send', 'event', 'contato', form_contato.id, 'preencheu');
-});
-form_contato[2].addEventListener("blur", function () {
-    console.log("heyheyhey blur telefone");
-    ga('send', 'event', 'contato', form_contato.id, 'preencheu');
-});
+function evento_pagina_analise(){
+    card_montadora = document.getElementsByClassName('card-montadoras')
+    card_montadora[0].addEventListener('click', function () {
+        console.log('Analise event sent');
+        ga('send', 'event', 'analise', 'ver_mais', card_montadora[0].dataset.name);
+    });
+    card_montadora[1].addEventListener('click', function () {
+        console.log('Analise event sent');
+        ga('send', 'event', 'analise', 'ver_mais', card_montadora[1].dataset.name);
+    });
+    card_montadora[2].addEventListener('click', function () {
+        console.log('Analise event sent');
+        ga('send', 'event', 'analise', 'ver_mais', card_montadora[2].dataset.name);
+    });
+}
 
-form_contato[4].addEventListener("click", function () {
-    console.log("heyheyhey click form");
-    ga('send', 'event', 'contato', 'enviado', 'enviado');
-});
+function evento_pagina_sobre(){
+    form_contato = document.getElementsByClassName('contato')[0]
+    form_contato[0].addEventListener('click', function () {
+        console.log('Sobre event sent');
+        ga('send', 'event', 'contato', form_contato.id, 'preencheu');
+    });
+    form_contato[1].addEventListener('click', function () {
+        console.log('Sobre event sent');
+        ga('send', 'event', 'contato', form_contato.id, 'preencheu');
+    });
+    form_contato[2].addEventListener('click', function () {
+        console.log('Sobre event sent');
+        ga('send', 'event', 'contato', form_contato.id, 'preencheu');
+    });
+
+    form_contato[4].addEventListener('click', function () {
+        console.log('Sobre event sent');
+        ga('send', 'event', 'contato', 'enviado', 'enviado');
+    });
+}
