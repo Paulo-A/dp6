b55e088647cfb2d2d1c2c5373152ff34141cabf3
Paulo-A <pauloeduardopandrade@gmail.com>
2021-01-09 19:36:59 -0300

    fix nomenclature

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -5,15 +5,15 @@
 // para fazer a sua coleta.
 // Caso tenha alguma dúvida sobre o case, não hesite em entrar em contato.
 
-path = window.location.pathname;
+const path = window.location.pathname;
 if (path === '/analise'){
-    event_analysis_page();
+    eventAnalysisPage();
 } else if (path === '/sobre'){
-    event_about_page();
+    eventAboutPage();
 }
-event_default();
+eventDefault();
 
-function event_default() {
+function eventDefault() {
     document.getElementsByClassName('menu-lista-contato')[0].addEventListener('click', function(){
         console.log('Contato event sent');
         ga('send', 'event', 'menu', 'entre_em_contato', 'link_externo');
@@ -25,32 +25,32 @@ function event_default() {
     });
 };
 
-function event_analysis_page(){
-    card = document.getElementsByClassName('card-montadoras');
-    card[0].addEventListener('click', analysis_field_click(card[0].dataset.name));
-    card[1].addEventListener('click', analysis_field_click(card[1].dataset.name));
-    card[2].addEventListener('click', analysis_field_click(card[2].dataset.name));
+function eventAnalysisPage(){
+    const card = document.getElementsByClassName('card-montadoras');
+    card[0].addEventListener('click', analysisFieldClick(card[0].dataset.name));
+    card[1].addEventListener('click', analysisFieldClick(card[1].dataset.name));
+    card[2].addEventListener('click', analysisFieldClick(card[2].dataset.name));
 };
 
-function analysis_page_click(event_label) {
+function analysisFieldClick(event_label) {
     console.log('Analise event sent');
     ga('send', 'event', 'analise', 'ver_mais', event_label);
 };
 
-function event_about_page(){
-    form_contact = document.getElementsByClassName('contato')[0]
+function eventAboutPage(){
+    const formContact = document.getElementsByClassName('contato')[0]
 
-    form_contact[0].addEventListener('blur', form_field_fill(form_contact.id));
-    form_contact[1].addEventListener('blur', form_field_fill(form_contact.id));
-    form_contact[2].addEventListener('blur', form_field_fill(form_contact.id));
+    formContact[0].addEventListener('blur', formFieldFill(formContact.id));
+    formContact[1].addEventListener('blur', formFieldFill(formContact.id));
+    formContact[2].addEventListener('blur', formFieldFill(formContact.id));
 
-    form_contact[4].addEventListener('submit', function () {
+    formContact[4].addEventListener('submit', function () {
         console.log('Sobre event sent');
         ga('send', 'event', 'contato', 'enviado', 'enviado');
     });
 };
 
-function form_field_fill(event_action){
+function formFieldFill(event_action){
     console.log('Sobre event sent');
     ga('send', 'event', 'contato', event_action, 'preencheu');
 };
